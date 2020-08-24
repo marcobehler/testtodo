ed35274368cf0476a2689b9720fd3cfe1dc7399c
marco <marco@marcobehler.com>
2020-08-24 16:44:02 +0200

    initial commit

---
+++ src/main/java/com/marcobehler/testtodo/TesttodoApplication.java
@@ -0,0 +1,13 @@
+package com.marcobehler.testtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesttodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesttodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcobehler/testtodo/TesttodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcobehler.testtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesttodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
