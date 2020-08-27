29265a7a38f4c4de1a3ba1277a53395b56436f62
marco <marco@marcobehler.com>
2020-08-27 09:23:52 +0200

    push

--- src/test/java/com/marcobehler/testtodo/TesttodoApplicationTests.java
+++ src/test/java/com/marcobehler/testtodo/TesttodoApplicationTests.java
@@ -3,11 +3,18 @@ package com.marcobehler.testtodo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class TesttodoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void bla() {
+		assertEquals(1,2);
 	}
 
 }
